2e14e19b7909bb1487f69f1a9aea01eee31605c5
PangolinPaw <PangolinPaw@users.noreply.github.com>
2018-01-27 20:33:13 +0000

    Initial framework

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <title>Working Title | Projects</title>
+        <?php include '/modules/head.php'; ?>
+    </head>
+    <body class="very-light">
+        <div class="nav very-dark">
+            <h1 class="light-font">Working Title <span class="very-light-font">|</span> <span class="medium-font">Projects</span></h1>
+        </div>
+        <div class="container">
+
+            <!-- Project Tile start -->
+            <div class="tile white">
+                <div class="name medium">
+                    <h2>Motor 1.5 reskin</h2>
+                </div>
+                <div class="body">
+                    <div class="progress system">
+                        <div class="dark"></div>
+                        <div class="dark"></div>
+                        <div class="light"><span>In progress</span></div>
+                        <div class="light"></div>
+                        <div class="light"></div>
+                        <div class="light"></div>
+                    </div>
+
+                    <div class="assigned">
+                        <div class="dark">GM</div>
+                        <div class="dark">DJ</div>
+                    </div>
+
+                    <div class="tags">
+                        <div class="dark"><span><i class="fa fa-tag" aria-hidden="true"></i> Tag 1</span></div>
+                        <div class="dark"><span><i class="fa fa-tag" aria-hidden="true"></i> Tag 2</span></div>
+                    </div>
+                </div>
+            </div>
+            <!-- Project Tile end -->
+
+            
+
+        </div>
+    </body>
+</html>
