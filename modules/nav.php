e0c0ed7d734955d55e9f8127f31f9db90452604d
PangolinPaw <PangolinPaw@users.noreply.github.com>
2018-03-11 13:25:10 +0000

    Add project creation functionality

--- modules/nav.php
+++ modules/nav.php
@@ -19,7 +19,6 @@
             $mention_icon = 'far fa-comment';
         }
     }
-
 ?>
 <script type="text/javascript">
     // Info: https://github.com/showdownjs/showdown
@@ -38,26 +37,49 @@
     }
 </script>
 
-<div class="popup_background" id="popup_background">
-    <div class="popup accent-border" id="new_popup">
+<div class="popup_background" id="popup_background" style="display: none;">
+    <div class="popup accent-border" id="new_popup" style="display: none;">
         <div class="name">
             <h2>Create a New project</h2>
             <span class="close_button" onclick="document.getElementById('popup_background').style.display='none'; document.getElementById('new_popup').style.display='none'"><i class="fas fa-times"></i></span>
         </div>
-        <div class="body">
-            <form id="new_project" method="post">
-                <input id="new_name" type="text" name="new_name" placeholder="Project title">
-                <textarea class="new_body" id="new_body_markdown" name="new_body" placeholder="Project brief" style="display: none;"></textarea>
-                <div class="new_body" id="new_body_html">
+        <form method="post">
+            <div class="body">
+                    <input id="new_name" type="text" name="new_name" placeholder="Project title">
+                    <textarea class="new_body" id="new_body_markdown" name="new_body" placeholder="Project brief"></textarea>
+                    <div class="new_body" id="new_body_html" style="display: none;">
+                    </div>
+            </div>
+            <div class="confirm_cancel_buttons">
+                <div class="left">
+                    Presets: <span onclick="document.getElementById('new_body_markdown').value='### Digital asset request\n\n#### Summary\n[Enter summary here]\n\n#### Required by\n[Date]\n\n#### Key stakeholders\n[Requested by & other stakeholders]\n\n#### Detailed requirements\n[Details]\n\n#### Other notes\n[Notes]'">Digital asset</span> | <span onclick="document.getElementById('new_body_markdown').value='### Main title\n\n#### Subtitle\n\n- bullet\n- point\n- list\n\n[link](www.google.co.uk)'">General request</span>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    Type:&nbsp;
+                    <select name="new_type">
+                        <option value="NORMAL">Normal</option>
+                        <option value="PRINT">Printed</option>
+                    </select>
                 </div>
-            </form>
-        </div>
-        <div class="confirm_cancel_buttons">
-            <span>Preview</span>
-            <button>Save</button>
-        </div>
-    </div>
-</div>
+                <div class="right">
+                    <span id="new_preview_link" onclick="
+                        document.getElementById('new_body_html').style.display='block'; 
+                        document.getElementById('new_body_markdown').style.display='none';
+                        document.getElementById('new_preview_link').style.display='none';
+                        document.getElementById('new_edit_link').style.display='inline-block';
+                        convert('new_body_markdown', 'new_body_html')
+                    ">Preview</span>
+                    <span id="new_edit_link" style="display: none" onclick="
+                        document.getElementById('new_body_html').style.display='none'; 
+                        document.getElementById('new_body_markdown').style.display='block';
+                        document.getElementById('new_edit_link').style.display='none';
+                        document.getElementById('new_preview_link').style.display='inline-block';
+                    ">Edit</span>
+                    <button>Save</button>
+                </div> <!-- /right -->
+            </div>
+        </form>
+    </div> <!-- /popup -->
+</div> <!-- /popup-background -->
 
 <div class="nav very-dark">
     <h1 class="accent-font"><a href="/" style="text-decoration: none; color: inherit;">Working Title</a> <span class="white-font">|</span> <span class="medium-font">All Projects</span></h1>
@@ -69,4 +91,34 @@
     		<li><span class="medium-font"><i class="fa fa-share-square" aria-hidden="true" title="Coming soon: Export summary"></i></span></li>
     	</ul>
     </div>
-</div>
+</div> <!-- /nav -->
+
+<?php 
+    // == FORM SUBMISSION ==
+    if ( isset($_POST['new_name']) ) {
+        $new_project_name = $_POST['new_name'];
+        $new_project_body = $_POST['new_body'];
+        $new_project_type = $_POST['new_type'];
+
+        if ( $new_project_name != '' and $new_project_body != '' ) {
+            $success = $db->exec('INSERT INTO projects (PROJECT_NAME, PROJECT_BODY, PROJECT_TYPE) VALUES ("' . $new_project_name .'", "' . $new_project_body . '", "' . $new_project_type . '")');
+            if ( $success ) {
+                $new_project_id = $db->query('SELECT PROJECT_ID FROM projects WHERE PROJECT_NAME="' . $new_project_name . '" ORDER BY PROJECT_ID DESC LIMIT 1');
+                $new_project_id = intval($new_project_id->fetchArray()[0]);
+                echo '<div id="alert" class="alert good" onclick="document.getElementById(\'alert\').style.display=\'none\';">
+                        <span><strong>New projet created.</strong> <a href="project.php?id=' . $new_project_id . '">Click here</a> to view, attach files and assign team members.</span>
+                    </div>';
+
+            } else {
+                echo '<div id="alert" class="alert bad" onclick="document.getElementById(\'alert\').style.display=\'none\';">
+                        <span><strong>Project creation failed:</strong> Unable to write to database, please try again</span>
+                    </div>';
+            }
+        } else {
+            echo '<div id="alert" class="alert bad" onclick="document.getElementById(\'alert\').style.display=\'none\';">
+                        <span><strong>Project creation failed:</strong> Please enter both a project title and brief</span>
+                    </div>';
+        }
+    }
+    // == FORM SUBMISSION END ==
+?>
--- project.php
+++ project.php
@@ -18,6 +18,12 @@
 
 
             // == MAIN QUERIES START ==
+            $project_array = $db->query('SELECT PROJECT_NAME, PROJECT_BODY, PROJECT_TYPE FROM projects WHERE PROJECT_ID=' . $project_id);
+            $project_array = $project_array->fetchArray();
+
+            $project_name = $project_array[0];
+            $project_body = $project_array[1];
+            $project_type = $project_array[2];
             
             // == MAIN QUERIES END ==
         ?>
@@ -38,101 +44,98 @@
                 
                 <div class="accent-border" style="width: 100%; height: 2em; border-bottom: 1px solid #cecece">
 
-                    <div style="width:1em; height: 100%; background: #a5a5a5; margin-left: -1em; float: left;">
-                        
-                        <img style="height: 50%; position: relative; transform: translateY(50%);" src="https://d30y9cdsu7xlg0.cloudfront.net/png/344330-200.png">
-
-                    </div>
-
                     <a href="project.php?id=' .  $project_id . '">
-                        <h2 style="font-size: 1.2em; line-height: 0.2em; margin-left: 0.5em;">Test</h2>
+                        <h2 style="font-size: 1.2em; line-height: 0.2em; margin-left: 0.5em;">
+                            <?php
+                                echo $project_name;
+                            ?>        
+                        </h2>
                     </a>
                 </div>
 
                 <table style="height: 5em; width: 100%;" >
                 <tbody>
                 <tr>
-                <td style="width: 40%; border-right: 1px solid #cecece;">&nbsp;</td>
-                <td style="width: 15%; border-right: 1px solid #cecece;">&nbsp;</td>
-                <td style="border-right: 1px solid #cecece;">
-                    <select name="select2" multiple="multiple">
-                        <option value="11">eleven</option>
-                        <option value="12">twelve</option>
-                        <option value="13">thirette</option>
-                        <option value="14">fourteen</option>
-                        <option value="15">fifteen</option>
-                        <option value="16">sixteen</option>
-                        <option value="17">seventeen</option>
-                        <option value="18">eighteen</option>
-                        <option value="19">nineteen</option>
-                        <option value="20">twenty</option>
-                      </select> 
+                <td style="width: 45%; border-right: 1px solid #cecece;">&nbsp;</td>
+                <td style="width: 15%; border-right: 1px solid #cecece;">
+                    <div style="margin: -0.5em 0 0.5em 0.5em;">Deadline</div>
                 </td>
-                <td>&nbsp;</td>
+                <td style="width: 20%; border-right: 1px solid #cecece;">
+                    <div style="margin: -0.5em 0 0.5em 0.5em;">Product owners</div>
+                    <?php
+                        echo '
+                                    <div class="assigned" style="padding-top: 0;">';
+                        $assigned_array = $db->query('SELECT assigned.USER_USERNAME, users.USER_NAME FROM assigned INNER JOIN users ON assigned.USER_USERNAME=users.USER_USERNAME WHERE assigned.PROJECT_ID=' . $project_id);
+                        while ( $assigned = $assigned_array->fetchArray() ) {
+                                echo '<a href="index.php?assigned=' .  $assigned[0] . '"><div><span><i class="fa fa-user" aria-hidden="true"></i>&nbsp;' . $assigned[1] . '</div></a>';
+                            }
+                        echo '      </div>';
+                    ?>
+                    <div style="float: right; font-size: 0.8em; margin: 2em 0.8em 0em 0em;">Add another..</div>
+                </td>
+
+                <td style="width: 20%;">
+                    <div style="margin: -0.5em 0 0.5em 0.5em;">Tags</div>
+                    <?php
+
+                    $all_tags = $db->query('SELECT TAG_TEXT FROM tags WHERE PROJECT_ID=' . $project_id);
+
+                        echo '
+                                    <div class="tags" style="padding-top: 0;">';
+                        while ( $tag = $all_tags->fetchArray() ) {
+                            $tag_text = $tag[0];
+                            echo '<a href="index.php?tag=' . $tag_text . '"><div><span><i class="fa fa-tag" aria-hidden="true"></i> ' . $tag_text  . '</span></div></a>';
+                        }
+                        echo '
+                                    </div>
+                                </div>
+                            </div>';
+                    ?>
+                    <div style="float: right; font-size: 0.8em; margin: 2em 0.8em 0em 0em;">Add another..</div>
+                </td>
                 </tr>
                 </tbody>
                 </table>
 
             </div>
 
-            <div class="tile white" style="height: 20em; width: 100%">
+            <div class="tile white accent-border" style="height: 20em; width: 100%">
                 
-                <div class="accent-border" style="width: 100%; height: 2em; border-bottom: 1px solid #969696;">
+                <!-- <div class="accent-border" style="width: 100%; height: 2em; border-bottom: 1px solid #969696;">
 
                 <table style="height: 1.5em; width: 100%;" >
                     <tbody>
                     <tr>
-                    <td style="width: 55%; border-right: 1px solid #cecece;">      
-                        <a href="project.php?id=' .  $project_id . '">
-                        <h2 style="font-size: 1.2em; line-height: 1.4em; margin: 0 0 0 0.5em;">Brief</h2>
+                    <td style="width: 70%; border-right: 1px solid #cecece;">
+                        <h2 style="hover:none; font-size: 1.2em; line-height: 1.4em; margin: 0 0 0 0.5em;">Brief</h2>
                         </a>
                     </td>
 
-                    <td style="width: 6.5%;">
-                        <a>
-                        <h2 style="font-size: 1.2em; line-height: 1.4em; margin: 0 0.5em 0 0.5em;">Preset:</h2>
-                        </a>
+                    <td>
+                        <div style="line-height: 1.4em; margin: 0 0.5em 0 0.5em;">Select print preset, test preset, or test preset.</div>
                     </td>
 
-                    <td style="width: 20.5%; border-right: 1px solid #cecece;">
-
-                    <form style="margin: 0;" action="/test.php">
-                              <select style="width: 15em;" name="preset">
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                              </select>
-                              <input type="submit">
-                            </form>
-
-                    </td>
-
-                    <td style="width: 9%; border-right: 1px solid #cecece;">
-                        
-                    </td>
-
-                    <td style="width: 9%;">&nbsp;</td>
                     </tr>
                     </tbody>
                 </table>
 
-                </div>
+                </div> -->
 
                 <div>
 
-                <textarea id="brief_markdown" style="width: 100%; height: 21.5em; border: none; resize: none; display: none">### test</textarea>
+                <textarea id="brief_markdown" style="width: 100%; height: 21em; border: none; resize: none; display: none"><?php echo $project_body; ?></textarea>
                 
                 <div id="brief_html" style="width: 100%; height: 19em; border: none; overflow-y: auto; display: block">
                     <img src="http://via.placeholder.com/350x150" onload="convert('brief_markdown', 'brief_html')">
                 </div>
 
-                <a id="edit_link" onclick="
+                <a style="float: right; margin-top: -3em; margin-right: 1em; cursor: pointer;" id="edit_link" onclick="
                     document.getElementById('brief_markdown').style.display = 'block';
                     document.getElementById('brief_html').style.display = 'none';
                     document.getElementById('edit_link').style.display = 'none';
                     document.getElementById('preview_link').style.display = 'block'"
                     >Edit</a>
-                <a id="preview_link" style="display: none" onclick="
+                <a style="float: right; margin-top: -1.5em; margin-right: 1em; display: none; cursor: pointer;" id="preview_link" onclick="
                     document.getElementById('brief_markdown').style.display = 'none';
                     document.getElementById('brief_html').style.display = 'block';
                     document.getElementById('preview_link').style.display = 'none';
